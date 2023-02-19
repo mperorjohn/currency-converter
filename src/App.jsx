a3910249597b939070505430d2564c2e49dcb83e
John <Mperorjohnoyekola@gmail.com>
2023-02-19 19:24:46 +0100

    Changs incoming

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,44 @@ import { useState } from 'react';
 
 function App() {
   // Defining the first state for input
-  const [seleFrom, setSelectFrom] = useState('')
-  const [input, setInput] = useState({});
-  const [tocur, setToCur] = useState('')
+  const [selectFrom, setSelectFrom] = useState('')
+  const [input, setInput] = useState(0);
+  const [toCur, setToCur] = useState('')
   const [result, setResult] = useState('')
+  const [currencies, setCurrencies] = useState('')
 
 
-  // first function to handle setform
-  function curhandler(e){
+  // first function to handle select from
+  const curhandler=(e)=>{
     setSelectFrom(e.target.value)
   }
   // second function to handle currency input
-  function handleInput(){
-    set
+  const handleInput=(e)=>{
+    setInput(e.target.value)
   }
+  // third function to handle tocur 
+  const handleTocur=(e)=>{
+    setToCur(e.target.value)
+
+  }
+  // fourth function to hande return result 
+  const handleResult=async()=>{
+    const config = {apikey:'Z5DCFr3bOYsHuDP5owbIew5n6UKf7v1J'}
+    const res = await axios.get(`https://api.apilayer.com/fixer/convert?to=${toCur}&from=${selectFrom}&amount=${input}`, config)
+    setResult(res)
+    console.log(res)
+
+  }
+  const getCurrencies=async()=>{
+    const config = {apikey:'Z5DCFr3bOYsHuDP5owbIew5n6UKf7v1J'}
+    const res = await axios.get('https://api.apilayer.com/fixer/sybol', config)
+    setResult(res)
+    console.log(res)
+
+  }
+
+
+  
 
   // const getcurrencies = async()=>{
   //   const cur = await
@@ -28,7 +52,7 @@ function App() {
                 <h1>Currency Converter</h1>
                 <p className='convert_from'>Convert From</p>
               <div>
-                <select name='from'>
+                <select name='from' onChange={curhandler}>
                   <option>Country</option> 
                   <option>Select two</option>     
                   <option>Select ten</option>   
@@ -39,23 +63,20 @@ function App() {
               </div>
               <p className='to'>To</p>
               <div >   
-                <select>
+                <select onChange={handleTocur}>
                   <option>Country</option> 
                   <option>Select two</option>     
                   <option>Select ten</option>   
 
-                </select> 
-                <div className='param'>
-
-                </div>
-              </div>
-                
+                </select>
+                <input type='number' className='param' />
                 <div>
 
-                <button type="button" className='convert' disabled={cur === ''}>Convert</button>
+                <button type="button" className='convert' onclick={handleResult} disabled={input === ''}>Convert</button>
                 </div>
+              </div>
+          </div>
       </div>
-    </div>
   );
 }
 
